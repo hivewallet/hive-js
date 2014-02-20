e08d6fb90057a91cd0d485000a58d37e2fb4ae0f
Wei Lu <luwei.here@gmail.com>
2014-02-20 21:50:19 +0800

    highlight selected tab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var profile = require('./pages/profile')
-
 profile(document.getElementById("profile"))
+
+// menu
+var $ = require('browserify-zepto');
+$(".tab").on('click', function(){
+  console.log(this, "clicked")
+  $('.tab').removeClass('active')
+  $(this).closest('.tab').addClass('active')
+})
