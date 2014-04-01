60a864f801ea009f729addd08f4bbf735e0180de
Wei Lu <luwei.here@gmail.com>
2014-04-01 13:26:58 +0800

    Show converted fiat currency balance
    exchange rates updates every 2 minutes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var landingDialog = require('./widgets/landing-dialog')
 var profile = require('./pages/profile')
 var apps = require('./pages/apps')
 var transactions = require('./pages/transactions')
+var Ticker = require('hive-ticker-api').BitcoinAverage
+var emitter = require('hive-emitter')
 
 menu(document.getElementById("sidebar"))
 profile(document.getElementById("profile"))
@@ -14,3 +16,15 @@ transactions(document.getElementById("transactions"))
 sendDialog(document.getElementById("send-dialog"))
 landingDialog(document.getElementById("landing-dialog"))
 
+
+function updateExchangeRates(){
+  var tickerUpdateInterval = 1000 * 60 * 2
+  var ticker = new Ticker()
+
+  ticker.getExchangeRates(function(err, rates){
+    emitter.emit('ticker', rates)
+    window.setTimeout(updateExchangeRates, tickerUpdateInterval)
+  })
+}
+
+updateExchangeRates()
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,6 +4,7 @@ var Ractive = require('ractify')
 var getWallet = require('hive-wallet').getWallet
 var emitter = require('hive-emitter')
 var Big = require('big.js')
+var currencies = require('hive-ticker-api').currencies
 
 module.exports = function(el){
   var ractive = new Ractive({
@@ -16,20 +17,34 @@ module.exports = function(el){
         email: 'wei@hivewallet.com',
         picture: 'https://pbs.twimg.com/media/BdrFa5WCUAAXFpZ.jpg'
       },
-      btcBalance: 'unknown',
+      currencies: currencies,
+      bitcoinBalance: 'unknown',
       fiatBalance: 'unknown',
-      satoshiToBTC: function(amount){
-        var satoshi = new Big(amount)
-        return satoshi.times(0.00000001)
-      }
+      satoshiToBTC: satoshiToBTC,
+      bitcoinToFiat: bitcoinToFiat
     }
   })
 
   emitter.on('wallet-ready', function(){
     var wallet = getWallet()
     ractive.set('user.address', wallet.currentAddress)
-    ractive.set('btcBalance', wallet.getBalance())
+    ractive.set('bitcoinBalance', wallet.getBalance())
   })
+
+  emitter.on('ticker', function(rates){
+    var currency = ractive.get('selectedFiat')
+    ractive.set('exchangeRate', rates[currency])
+  })
+
+  function satoshiToBTC(amount){
+    var satoshi = new Big(amount)
+    return satoshi.times(0.00000001)
+  }
+
+  function bitcoinToFiat(amount, exchangeRate){
+    var btc = satoshiToBTC(amount)
+    return btc.times(exchangeRate).toFixed(2)
+  }
 
   return ractive
 }
