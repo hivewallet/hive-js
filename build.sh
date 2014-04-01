780a7fc010b604e33fa34fe13b12d95b107b56ba
Wei Lu <luwei.here@gmail.com>
2014-04-01 10:37:57 +0800

    move mnemonic operations to a webworker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-aperture open
-browserify index.js -t ractify pages/*/**.js > public/bundle.js
+aperture link
+browserify index.js -t ractify > public/bundle.js
 sheetify index.css | node rework.js > public/stylesheets/bundle.css
