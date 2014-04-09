3873e94aab5ef30db9792daf56be65160e3c2d2b
Wei Lu <luwei.here@gmail.com>
2014-04-09 17:00:45 +0800

    encrypt user data before saving to db

--- app/js/lib/db/index.js
+++ app/js/lib/db/index.js
@@ -2,15 +2,22 @@ var emitter = require('hive-emitter')
 var PouchDB = require('pouchdb')
 var getWallet = require('hive-wallet').getWallet
 var $ = require('browserify-zepto')
+var CJS = require('crypto-js')
+var AES = CJS.AES
+var utf8Encode = CJS.enc.Utf8
 
 var db = new PouchDB('hive')
 var id = null
+var key = null
 
 function set(key, value, callback){
   if(id == null) return;
 
   db.get(id, function(err, doc){
-    $.extend(true, doc[key], value)
+    var data = JSON.parse(decrypt(doc.data))
+    $.extend(true, data[key], value)
+
+    doc.data = encrypt(JSON.stringify(data))
     db.put(doc, callback)
   })
 }
@@ -19,17 +26,28 @@ function get(key, callback) {
   if(id == null) return;
 
   db.get(id, function(err, doc){
-    var value = doc[key]
+    var data = JSON.parse(decrypt(doc.data))
+    var value = data[key]
     if(key instanceof Function){
-      value = doc
+      value = data
       callback = key
     }
     callback(err, value)
   })
 }
 
+function encrypt(text) {
+  return AES.encrypt(text, key).toString()
+}
+
+function decrypt(text) {
+  return utf8Encode.stringify(AES.decrypt(text, key))
+}
+
 emitter.on('wallet-ready', function(){
-  id = getWallet().id
+  var wallet = getWallet()
+  id = wallet.id
+  key = wallet.getSeed()
 
   db.get(id, function(err, doc){
     if(err) {
@@ -43,7 +61,6 @@ emitter.on('wallet-ready', function(){
 
 function initializeRecord(){
   var defaultValue = {
-    _id: id,
     systemInfo: { preferredCurrency: 'USD' },
     userInfo: {
       firstName: 'Firstname',
@@ -52,7 +69,12 @@ function initializeRecord(){
     }
   }
 
-  db.put(defaultValue, function(err, response){
+  var doc = {
+    _id: id,
+    data: encrypt(JSON.stringify(defaultValue))
+  }
+
+  db.put(doc, function(err, response){
     if(err) return console.error(err);
 
     emitter.emit('db-ready')
