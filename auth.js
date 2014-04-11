06a62004e3ab1cd5ecce9c1152a763ae46677a8a
Wei Lu <luwei.here@gmail.com>
2014-04-11 20:47:49 +0800

    login with fail counter

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 var cradle = require('cradle');
 var crypto = require('crypto');
 
+var userPrefix = "org.couchdb.user:"
+
 cradle.setup({
   host: process.env.DB_HOST,
   port: 80,
@@ -18,14 +20,26 @@ var conn = new (cradle.Connection)({
 
 var db = conn.database('_users')
 
-function register(name, password, callback){
+function register(name, pin, callback){
   db.get(name, function (err, doc) {
     if(err && err.error === 'not_found'){
-      createUser(name, password, callback)
+      createUser(name, pin, callback)
     } else if(err) {
       callback(err)
     } else {
       callback({error: 'user_exists'})
+    }
+  })
+}
+
+function login(name, pin, callback) {
+  name = userPrefix + name
+  db.get(name, function (err, doc) {
+    if(err){
+      console.error('error getting doc', err)
+      callback({error: 'auth_failed'})
+    } else {
+      verifyPin(doc, name, pin, callback)
     }
   })
 }
@@ -35,13 +49,14 @@ function createUser(name, pin, callback){
   var password = longPassword + pin
   var hashAndSalt = generatePasswordHash(password)
 
-  db.save("org.couchdb.user:" + name, {
+  db.save(userPrefix + name, {
     name: name,
     password_sha: hashAndSalt[0],
     salt: hashAndSalt[1],
     password_scheme: 'simple',
     type: 'user',
-    long_password: longPassword
+    long_password: longPassword,
+    failed_attempts: 0
   }, function(err, res){
     if(err) return callback(err);
     callback(null, longPassword)
@@ -59,6 +74,28 @@ function generatePasswordHash(password){
   return [hash.digest('hex'), salt];
 }
 
+function verifyPin(user, name, pin, callback) {
+  var password = user.long_password + pin
+  var hash = crypto.createHash('sha1')
+  var sha = hash.update(password + user.salt).digest('hex')
+  if(sha === user.password_sha) {
+    callback(null, user.long_password)
+  } else {
+    incrementFailCount(user, callback)
+  }
+}
+
+function incrementFailCount(user, callback) {
+  var counter = { failed_attempts: user.failed_attempts + 1 }
+  db.merge(user._id, counter, function(err, res){
+    if(err) {
+      console.error('FATAL: failed to increament counter')
+    }
+
+    callback({error: 'auth_failed'})
+  })
+}
+
 function getUser(name) {
   db.get(name, function (err, doc) {
     console.log(arguments)
@@ -66,5 +103,6 @@ function getUser(name) {
 }
 
 module.exports = {
-  register: register
+  register: register,
+  login: login
 }
--- server.js
+++ server.js
@@ -9,14 +9,9 @@ var app = express()
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.post('/register', function(req, res) {
+app.post('/register', validate_params, function(req, res) {
   var name = req.body.wallet_id
-  var password = req.body.pin
-  if (!name || !password) {
-    return res.send(400, 'Bad request');
-  }
-
-  auth.register(name, password, function(err, longPassword){
+  auth.register(name, req.body.pin, function(err, longPassword){
     if(err) {
       console.error('error', err)
       return res.send(500, err);
@@ -25,7 +20,27 @@ app.post('/register', function(req, res) {
     console.log('registered wallet %s', name)
     res.send(201, longPassword)
   })
-});
+})
+
+app.post('/login', validate_params, function(req, res) {
+  var name = req.body.wallet_id
+  auth.login(name, req.body.pin, function(err, longPassword){
+    if(err) {
+      console.error('error', err)
+      return res.send(500, err);
+    }
+
+    console.log('authenticated wallet %s', name)
+    res.send(200, longPassword)
+  })
+})
+
+function validate_params(req, res, next) {
+  if (!req.body.wallet_id || !req.body.pin) {
+    return res.send(400, 'Bad request');
+  }
+  next()
+}
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 9009, function() {
