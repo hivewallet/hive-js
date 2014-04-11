751bf36d8e58ca9288f60050d1867aaa9956ef7e
Wei Lu <luwei.here@gmail.com>
2014-04-11 18:15:53 +0800

    register user with wallet_id and pin

---
+++ auth.js
@@ -0,0 +1,71 @@
+var cradle = require('cradle');
+var crypto = require('crypto');
+
+cradle.setup({
+  host: process.env.DB_HOST,
+  port: 80,
+  cache: false,
+  timeout: 5000
+})
+
+var conn = new (cradle.Connection)({
+  secure: true,
+  auth: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  }
+})
+
+var db = conn.database('_users')
+
+function register(name, password, callback){
+  db.get(name, function (err, doc) {
+    if(err && err.error === 'not_found'){
+      createUser(name, password, callback)
+    } else if(err) {
+      console.error('get error', err)
+      callback(err)
+    } else {
+      callback({error: 'user_exists'})
+    }
+  })
+}
+
+function createUser(name, password, callback){
+  var hashAndSalt = generatePasswordHash(password)
+  var longPassword = generateLongPassword()
+
+  db.save("org.couchdb.user:" + name, {
+    name: name,
+    password_sha: hashAndSalt[0],
+    salt: hashAndSalt[1],
+    password_scheme: 'simple',
+    type: 'user',
+    long_password: longPassword
+  }, function(err, res){
+    if(err) return callback(err);
+    callback(null, longPassword)
+  })
+}
+
+function generateLongPassword(){
+  return crypto.randomBytes(64).toString('hex');
+}
+
+function generatePasswordHash(password){
+  var salt = crypto.randomBytes(16).toString('hex');
+  var hash = crypto.createHash('sha1');
+  hash.update(password + salt);
+  hash = '-hashed-' + hash.digest('hex') + ',' + salt
+  return [hash, salt];
+}
+
+function getUser(name) {
+  db.get(name, function (err, doc) {
+    console.log(arguments)
+  })
+}
+
+module.exports = {
+  register: register
+}
--- server.js
+++ server.js
@@ -3,11 +3,33 @@
 var express = require('express')
 var http = require('http')
 var path = require('path')
+var auth = require('./auth')
 
 var app = express()
+app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'build')))
+
+app.post('/register', function(req, res) {
+  console.log('body', req.body)
+  var name = req.body.wallet_id
+  var password = req.body.pin
+  if (!name || !password) {
+    return res.send(400, 'Bad request');
+  }
+
+  auth.register(name, password, function(err, longPassword){
+    if(err) {
+      console.error('error', err)
+      return res.send(500, err);
+    }
+
+    console.log('registered wallet %s', name)
+    res.send(201, longPassword)
+  })
+});
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 9009, function() {
   console.info('server listening on http://localhost:' + server.address().port)
 });
+
