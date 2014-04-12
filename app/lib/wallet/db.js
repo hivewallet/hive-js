e0a2ac5188c6300495690e00a2ac166285adc866
Wei Lu <luwei.here@gmail.com>
2014-04-12 21:57:07 +0800

    automatically overwrite existing credentials in local db

--- app/lib/wallet/db.js
+++ app/lib/wallet/db.js
@@ -4,16 +4,26 @@ var PouchDB = require('pouchdb')
 var db = new PouchDB('hive-local')
 var AES = require('hive-aes')
 
+var credentials = "credentials"
+
 function saveSeed(id, seed, token, callback) {
-  var encryptedSeed = AES.encrypt(seed, token)
+  db.get(credentials, function(err, doc){
+    if(doc) {
+      return db.remove(doc, function(err, doc){
+        if(err) return callback(err);
 
-  var doc = {
-    _id: "credentials",
-    id: id,
-    seed: encryptedSeed
-  }
+        saveSeed(id, seed, token, callback)
+      })
+    }
 
-  db.put(doc, callback)
+    var encryptedSeed = AES.encrypt(seed, token)
+    var doc = {
+      _id: credentials,
+      id: id,
+      seed: encryptedSeed
+    }
+    db.put(doc, callback)
+  })
 }
 
 module.exports = {
