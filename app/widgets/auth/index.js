9ef828aa842c0200de03a9cf9210fa75f16eaafc
David Hauser <davidvonhauser@gmail.com>
2014-05-20 17:35:57 +1000

    separate create views into component

--- app/widgets/auth/index.js
+++ app/widgets/auth/index.js
@@ -3,6 +3,7 @@
 var Ractive = require('hive-ractive')
 var Hive = require('hive-wallet')
 var emitter = require('hive-emitter')
+var createView = require('hive-create_wallet')
 var router = require('hive-router').router
 var hasher = require('hive-router').hasher
 var $ = require('browserify-zepto')
@@ -33,111 +34,14 @@ function register(el){
 
   includeSharedBehaviors(ractive)
 
-  ractive.on('prepare-seed', function(event){
+  var createEl = createView(ractive.nodes.create_wallet)
+  var landingEl = $(ractive.nodes.landing_el)
+
+  ractive.on('create-new-wallet', function(event){
     event.original.preventDefault()
-    ractive.set('createWallet', true)
+    landingEl.removeClass('current')
+    createEl.show()
   })
-
-  ractive.on('temp-back', function(event){
-    event.original.preventDefault()
-    ractive.set('createWallet', false)
-  })
-
-  ractive.on('generate-phrase', function(event){
-    event.original.preventDefault()
-    ractive.set('create_intro', false)
-    ractive.set('create_read', true)
-    Hive.createWallet(null, ractive.getNetwork(), onSeedCreated)
-  })
-
-  function onSeedCreated() {
-    var wallet = Hive.getWallet()
-    var string = wallet.getMnemonic()
-    var array = string.split(' ')
-    ractive.set('passphrase', string)
-    ractive.set('passphrase_length', array.length)
-    ractive.set('current_word', 0)
-    ractive.set('passphrase_pending', false)
-    ractive.set('passphrase_array', array)
-
-    var current_element = $(ractive.nodes['seed_word_' + 0])
-    current_element.addClass('middle')
-
-    $(ractive.findAll('.attach_fastclick')).each(function(){
-      fastclick(this);
-    });
-  }
-
-  ractive.on('next-word', function(event) {
-
-    event.original.preventDefault()
-    var old_word = ractive.get('current_word')
-    var length = ractive.get('passphrase_array').length
-    var is_animating = ractive.get('passphrase_animating')
-
-    if(is_animating){ return; }
-    if(old_word === length - 1) {
-      ractive.set('create_read', false)
-      ractive.set('create_confirm', true)
-      return;
-    }
-    if(old_word === length - 2) {
-      ractive.set('pass_next_text', 'Review Passphrase')
-    }
-
-    ractive.set('passphrase_animating', true)
-
-    var new_word = old_word + 1;
-    var old_element = $(ractive.nodes['seed_word_' + old_word])
-    var new_element = $(ractive.nodes['seed_word_' + new_word])
-
-    ractive.set('current_word', new_word)
-    old_element.addClass('left')
-    new_element.addClass('middle')
-
-    // arrival(ractive.nodes.pass_words, animation_complete)
-    setTimeout(animation_complete, 400)
-  })
-
-  ractive.on('prev-word', function(event) {
-
-    event.original.preventDefault()
-    var old_word = ractive.get('current_word')
-    var length = ractive.get('passphrase_array').length
-    var is_animating = ractive.get('passphrase_animating')
-
-    if(old_word === 0 || is_animating){ return; }
-    if(old_word === length - 1) {
-      ractive.set('pass_next_text', 'Next word')
-    }
-
-    ractive.set('passphrase_animating', true)
-
-    var new_word = old_word - 1;
-    var old_element = $(ractive.nodes['seed_word_' + old_word])
-    var new_element = $(ractive.nodes['seed_word_' + new_word])
-
-    ractive.set('current_word', new_word)
-    old_element.removeClass('middle')
-    new_element.removeClass('left')
-
-    // arrival(ractive.nodes.pass_words, animation_complete)
-    setTimeout(animation_complete, 400)
-  })
-
-  function animation_complete() {
-    ractive.set('passphrase_animating', false)
-  }
-
-  ractive.on('create-pin', function(event) {
-    event.original.preventDefault()
-    ractive.set('create_confirm', false)
-    ractive.set('create_pin', true)
-  })
-
-
-
-
 
   function onWalletCreated() {
     ractive.pauseLoading()
@@ -167,9 +71,12 @@ function register(el){
     Hive.createWallet(null, ractive.getNetwork(), onWalletCreated)
   })
 
-  ractive.on('set-pin', function(event){
+  emitter.on('set-pin', function(){
+
     Hive.setPin(ractive.get('pin'), ractive.onSyncDone)
     ractive.set('progress', 'Saving pin...')
+    createEl.hide()
+    landingEl.addClass('current')
   })
 
   function getPassphrase(){
@@ -210,7 +117,6 @@ function login(el){
 
   return ractive
 }
-
 
 function includeSharedBehaviors(ractive) {
   emitter.on('wallet-opening', function(progress){
