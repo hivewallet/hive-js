be185dc58bf5a09578a063f858817d33244157d2
haustraliaer <davidvonhauser@gmail.com>
2014-05-25 00:16:13 +1000

    bring send into send tab

--- app/pages/receive/index.js
+++ app/pages/receive/index.js
@@ -1,10 +1,28 @@
 'use strict';
 
 var Ractive = require('hive-ractive')
+var emitter = require('hive-emitter')
+var initSearch = require('hive-search')
 
 module.exports = function(el){
-  return new Ractive({
+  var ractive = new Ractive({
     el: el,
     template: require('./index.ract').template,
   });
+
+  var search = initSearch(ractive.find("#search"))
+
+  ractive.updateFastclick()
+
+  ractive.on('send', function(event) {
+    event.original.preventDefault()
+    emitter.emit('open-send-dialog')
+  })  
+
+  ractive.on('error', function(event) {
+    event.original.preventDefault()  
+    emitter.emit('open-error')
+  })
+
+  return ractive;
 }
--- app/pages/send/index.js
+++ app/pages/send/index.js
@@ -1,29 +1,78 @@
 'use strict';
 
-var $ = require('browserify-zepto')
 var Ractive = require('hive-ractive')
+var getWallet = require('hive-wallet').getWallet
+var Big = require('big.js')
 var emitter = require('hive-emitter')
-var initSearch = require('hive-search')
+var db = require('hive-db')
 
 module.exports = function(el){
   var ractive = new Ractive({
     el: el,
-    template: require('./index.ract').template
+    template: require('./index.ract').template,
+    data: {
+      exchangeRates: {}
+    }
   })
 
-  var search = initSearch(ractive.find("#search"))
-
-  ractive.updateFastclick()
-
-  ractive.on('send', function(event) {
-    event.original.preventDefault()
-    emitter.emit('open-send-dialog')
-  })  
-
-  ractive.on('error', function(event) {
-    event.original.preventDefault()  
-    emitter.emit('open-error')
+  db.get('systemInfo', function(err, info){
+    if(err) return console.error(err);
+    ractive.set('fiatCurrency', info.preferredCurrency)
   })
+
+  ractive.on('send', function(event){
+    var to = ractive.get('to')
+    var value = bitcoinToSatoshi(ractive.get('value'))
+
+    var wallet = getWallet()
+
+    wallet.createTxAsync(to, value, function(err, tx){
+      if(err) return alert(err)
+
+      ractive.set('visible', false)
+      emitter.emit('close-send-dialog')
+      ractive.set('hide_address', false)
+      wallet.sendTx(tx, onTxSent)
+    })
+  })
+
+  ractive.on('fiat-to-bitcoin', function(event){
+    var fiat = event.node.value
+    if(fiat === '') return;
+
+    var exchangeRate = ractive.get('exchangeRates')[ractive.get('fiatCurrency')]
+    var bitcoin = new Big(fiat).div(exchangeRate).toFixed(8)
+
+    ractive.set('value', bitcoin)
+  })
+
+  ractive.on('bitcoin-to-fiat', function(event){
+    var bitcoin = event.node.value
+    if(bitcoin === '') return;
+
+    var exchangeRate = ractive.get('exchangeRates')[ractive.get('fiatCurrency')]
+    var fiat = new Big(bitcoin).times(exchangeRate).toFixed(2)
+
+    ractive.set('fiatValue', fiat)
+  })
+
+  emitter.on('ticker', function(rates){
+    ractive.set('exchangeRates', rates)
+  })
+
+  function bitcoinToSatoshi(amount){
+    var btc = new Big(amount)
+    return parseInt(btc.times(100000000).toFixed(0))
+  }
+
+  function onTxSent(err, transaction){
+    if(err) return alert("error sending transaction. " + err)
+
+    // update balance & tx history
+    emitter.emit('wallet-ready')
+    emitter.emit('transactions-loaded', [transaction])
+  }
+
 
   return ractive
 }
