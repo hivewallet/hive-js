6f805cd805f9e0e529e9fffe97b7b0d8e2994905
Wei Lu <luwei.here@gmail.com>
2014-05-29 11:33:18 +0800

    show qrcode of current address
    [#14]

--- app/pages/receive/index.js
+++ app/pages/receive/index.js
@@ -1,16 +1,36 @@
-'use strict';
+'use strict'
 
 var Ractive = require('hive-ractive')
 var emitter = require('hive-emitter')
+var qrcode = require('hive-qrcode')
+var Hive = require('hive-wallet')
 
 module.exports = function(el){
   var ractive = new Ractive({
     el: el,
     template: require('./index.ract').template,
     data: {
-      address: '1Bgbigq3ivsCUgAG3s…'
+      address: '',
+      qrVisible: false
     }
-  });
+  })
 
-  return ractive;
+  emitter.on('wallet-ready', function(){
+    ractive.set('address', getAddress())
+  })
+
+  ractive.on('show-qr', function(){
+    ractive.set('qrVisible', true)
+
+    var qr = qrcode('bitcoin:' + getAddress())
+    var container = ractive.find('#qrcontainer')
+    container.innerHTML = ''
+    container.appendChild(qr)
+  })
+
+  function getAddress(){
+    return Hive.getWallet().currentAddress
+  }
+
+  return ractive
 }
