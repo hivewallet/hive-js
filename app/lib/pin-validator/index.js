2390a8f5fa0e7d6ef29864f7b8f1cf2c103a14e7
Wei Lu <luwei.here@gmail.com>
2014-05-29 14:00:18 +0800

    Add pin validator module
    [#45]

---
+++ app/lib/pin-validator/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function validate(pin) {
+  var pinNumber = parseInt(pin, 10)
+  if(isNaN(pinNumber)) return false;
+
+  return pin.length === 4
+}
+
+module.exports = validate
---
+++ app/lib/pin-validator/test/index.js
@@ -0,0 +1,27 @@
+var validate = require('../index')
+
+describe('pin validator', function(){
+  it('returns false if undefined', function(){
+    expect(validate()).toBeFalsy()
+  })
+
+  it('returns false for empty string', function(){
+    expect(validate('')).toBeFalsy()
+  })
+
+  it('returns false if not a number', function(){
+    expect(validate('aaaa')).toBeFalsy()
+  })
+
+  it('returns false if less than 4 digits', function(){
+    expect(validate('111')).toBeFalsy()
+  })
+
+  it('returns false if more than 4 digits', function(){
+    expect(validate('11111')).toBeFalsy()
+  })
+
+  it('returns true if valid', function(){
+    expect(validate('1111')).toBeTruthy()
+  })
+})
