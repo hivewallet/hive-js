66e3004d238b114ea8d72f9523ac054059b69afe
Wei Lu <luwei.here@gmail.com>
2014-06-10 11:04:21 +0800

    Convert functions handles undefined and empty strings

--- app/lib/convert/index.js
+++ app/lib/convert/index.js
@@ -1,8 +1,10 @@
 function btcToSatoshi(btc) {
+  if(btc == undefined || btc === '') return;
   return btc * 100000000
 }
 
 function satoshiToBtc(satoshi) {
+  if(satoshi == undefined || satoshi === '') return;
   return satoshi / 100000000
 }
 
--- app/lib/convert/test/index.js
+++ app/lib/convert/test/index.js
@@ -10,6 +10,12 @@ describe('convert', function(){
       expect(convert.btcToSatoshi(1)).toEqual(100000000)
       expect(convert.btcToSatoshi(0.005)).toEqual(500000)
     })
+
+    it('returns undefined for empty value', function(){
+      expect(convert.btcToSatoshi()).toEqual(undefined)
+      expect(convert.btcToSatoshi(null)).toEqual(undefined)
+      expect(convert.btcToSatoshi('')).toEqual(undefined)
+    })
   })
 
   describe('satoshiToBtc', function(){
@@ -20,6 +26,12 @@ describe('convert', function(){
     it('works for non-zero', function(){
       expect(convert.satoshiToBtc(12345)).toEqual(0.00012345)
       expect(convert.satoshiToBtc(123456789)).toEqual(1.23456789)
+    })
+
+    it('returns undefined for empty value', function(){
+      expect(convert.satoshiToBtc()).toEqual(undefined)
+      expect(convert.satoshiToBtc(null)).toEqual(undefined)
+      expect(convert.satoshiToBtc('')).toEqual(undefined)
     })
   })
 })
