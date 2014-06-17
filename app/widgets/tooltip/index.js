4d3cbe5a443db5fbe230ff3ed493f789e9d07228
haustraliaer <davidvonhauser@gmail.com>
2014-06-17 14:18:50 +1000

    wire up tooltip widget

---
+++ app/widgets/tooltip/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Ractive = require('hive-modal')
+var emitter = require('hive-emitter')
+
+module.exports = function showTooltip(data){
+
+  var ractive = new Ractive({
+    el: document.getElementById('tooltip'),
+    partials: {
+      content: require('./content.ract').template,
+    },
+    data: data
+  })
+
+  return ractive
+}
+
