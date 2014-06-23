28b99286d0b53b8f4ca221c4a6ad0ebb62440b09
Matt Lenz <matt@mattlenz.com>
2014-06-23 14:11:35 +1000

    Add cordova geolocation plugin

--- cordova/platforms/ios/www/cordova_plugins.js
+++ cordova/platforms/ios/www/cordova_plugins.js
@@ -41,6 +41,34 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -50,7 +78,8 @@ module.exports.metadata =
     "com.phonegap.plugins.barcodescanner": "1.0.1",
     "org.apache.cordova.device": "0.2.10",
     "org.apache.cordova.splashscreen": "0.3.1",
-    "org.apache.cordova.console": "0.2.10-dev"
+    "org.apache.cordova.console": "0.2.10-dev",
+    "org.apache.cordova.geolocation": "0.3.9-dev"
 }
 // BOTTOM OF METADATA
 });
