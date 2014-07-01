8a88e02c1efc14a9134e3329fdcc063f7f2211ed
Wei Lu <luwei.here@gmail.com>
2014-07-01 01:44:03 -0700

    Validator accounts for insufficient balance & attempt to empty wallet without fee

--- app/lib/wallet/test/index.js
+++ app/lib/wallet/test/index.js
@@ -4,12 +4,13 @@ var validateSend = require('../validator')
 
 describe('validate', function(){
   var wallet = new Wallet(new Buffer('ff', 'hex'), networks.testnet)
+  var to = 'mqMRh2h5QbLyD4K3jZQMk2DzRwf2GwSoSQ'
   var from = wallet.generateAddress()
   var utxo = {
     "hash":"6a4062273ac4f9ea4ffca52d9fd102b08f6c32faa0a4d1318e3a7b2e437bb9c7",
     "outputIndex": 0,
     "address" : from,
-    "value": 20000
+    "value": 29999
   }
   wallet.setUnspentOutputs([utxo])
 
@@ -29,7 +30,7 @@ describe('validate', function(){
     })
 
     it('allows valid address', function(done){
-      validateSend(wallet, 'mqMRh2h5QbLyD4K3jZQMk2DzRwf2GwSoSQ', 0.0001, function(err){
+      validateSend(wallet, to, 0.0001, function(err){
         expect(err).toBeUndefined()
         done()
       })
@@ -38,10 +39,39 @@ describe('validate', function(){
 
   describe('send amount', function(){
     it('must be above dust threshold', function(done){
-      validateSend(wallet, 'mqMRh2h5QbLyD4K3jZQMk2DzRwf2GwSoSQ', 0.00000546, function(err){
+      validateSend(wallet, to, 0.00000546, function(err){
         expect(err.message).toEqual('Please enter an amount above 0.00000546')
         done()
       })
+    })
+
+    it('catches amount more than wallet balance', function(done){
+      validateSend(wallet, to, 0.0003, function(err){
+        expect(err.message).toEqual("You don't have enough funds in your wallet")
+        done()
+      })
+    })
+
+    describe('trying to empty wallet without including fee', function(){
+      it('amount equals balance exactly', function(done){
+        validateSend(wallet, to, 0.00029999, function(err){
+          expect(err.message).toEqual(getExpectedMessage(0.00019999))
+          expect(err.sendableBalance).toEqual(0.00019999)
+          done()
+        })
+      })
+
+      it('balance - feePerKb < amount < balance', function(done){
+        validateSend(wallet, to, 0.0002, function(err){
+          expect(err.message).toEqual(getExpectedMessage(0.00019999))
+          expect(err.sendableBalance).toEqual(0.00019999)
+          done()
+        })
+      })
+
+      function getExpectedMessage(amount){
+        return "It seems like you are trying to empty your wallet. Taking transaction fee into account, we estimated that the max amount you can send is " + amount + ". We have amended the value in the amount field for you."
+      }
     })
   })
 
--- app/lib/wallet/validator.js
+++ app/lib/wallet/validator.js
@@ -2,6 +2,7 @@ var Address = require('bitcoinjs-lib').Address
 var assert = require('assert')
 var btcToSatoshi = require('hive-convert').btcToSatoshi
 var satoshiToBtc = require('hive-convert').satoshiToBtc
+var toFixedFloor = require('hive-convert').toFixedFloor
 
 function validateSend(wallet, to, btcValue, callback){
   var amount = btcToSatoshi(btcValue)
@@ -21,12 +22,42 @@ function validateSend(wallet, to, btcValue, callback){
 
     if(message.match(/dust threshold/)) {
       message = 'Please enter an amount above ' + satoshiToBtc(network.dustThreshold)
+    } else if(message.match(/Not enough funds/)) {
+      if(attemptToEmptyWallet()){
+        var sendableBalance = satoshiToBtc(amount - getShortAmount(message))
+
+        message = [
+          "It seems like you are trying to empty your wallet.",
+          "Taking transaction fee into account, we estimated that the max amount you can send is",
+          sendableBalance + ".",
+          "We have amended the value in the amount field for you."
+        ].join(' ')
+
+        var error = new Error(message)
+        error.sendableBalance = sendableBalance
+
+        return new callback(error)
+      } else {
+        message = "You don't have enough funds in your wallet"
+      }
     }
 
     return new callback(new Error(message))
   }
 
   callback()
+
+  function attemptToEmptyWallet(){
+    var balance = wallet.getBalance()
+    return balance - network.feePerKb < amount && amount <= balance
+  }
+
+  function getShortAmount(message){
+    var values = message.replace(/[^\d]+(\d+)[^\d]+(\d+)/, "$1 $2").split(' ').map(function(m){
+      return parseInt(m)
+    })
+    return values[1] - values[0]
+  }
 }
 
 module.exports = validateSend
