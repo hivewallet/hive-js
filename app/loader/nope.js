be498c9da70eb890db4ec6764208472492caaff7
haustraliaer <davidvonhauser@gmail.com>
2014-07-11 21:49:43 +1000

    Text adjustment

--- app/loader/nope.js
+++ app/loader/nope.js
@@ -1,2 +1,2 @@
-var message = "Sorry, Hive Wallet didn't load. <br/><br/> Try updating your browser, or switching out of private browsing mode. If all else fails, download Chrome."
+var message = "Sorry, Hive Wallet didn't load. <br/><br/> Try updating your browser, or switching out of private browsing mode. If all else fails, download Chrome for your device."
 document.getElementById('loader-message').innerHTML = message
