9cda6f45fd743f68d13d3d14414d5ad5bc174498
Wei Lu <luwei.here@gmail.com>
2014-07-30 14:04:22 +0800

    Add loader to build script

--- build.js
+++ build.js
@@ -20,27 +20,48 @@ function scripts(callback) {
   bundle('./app/application.js', './build/assets/js/application.js', callback)
 }
 
+function loader(callback) {
+  bundle('./app/loader/nope.js', './build/assets/js/nope.js', callback)
+  bundle('./app/loader/index.js', './build/assets/js/loader.js', callback)
+}
+
 function bundle(inFile, outFilename, callback){
   watchify.args.entries = path.join(__dirname, inFile)
   var bundler = browserify(watchify.args)
 
+  // watch
   if(!isProduction()){
     bundler = watchify(bundler)
     bundler.on('update', function(ids){
-      console.log(outFilename, 'updated due to changes in', ids)
+      console.log(inFile, 'updated due to changes in', ids)
     })
   }
 
+  // transforms
   bundler = bundler.transform('ractify')
   if(isProduction()) {
     bundler = bundler.transform({global: true}, 'uglifyify')
   }
 
+  // bundle
   var dest = fs.createWriteStream(outFilename);
   bundler.bundle()
-    .on('error', callback)
-    .on('end', callback)
+    .on('error', onError)
+    .on('end', onEnd)
     .pipe(dest)
+
+  function onError(err){
+    console.error(inFile, "compilation failed")
+    console.error(err.message);
+    console.error(err.stack)
+
+    if(callback) callback(err)
+  }
+
+  function onEnd(){
+    console.log(inFile, "compiled")
+    if(callback) callback()
+  }
 }
 
 function isProduction(){
@@ -54,13 +75,12 @@ function done(filename, err){
     console.error(err.stack)
     return
   }
-  console.log(filename, "was reloaded")
+  console.log(filename, "compiled")
 }
 
 styles(function(err){
   done('application.css')
 })
 
-scripts(function(err){
-  done('application.js')
-})
+scripts()
+loader()
