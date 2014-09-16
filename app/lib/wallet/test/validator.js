e26c41c931adb8e17659565b6c72f373ac3b8ad3
Wei Lu <luwei.here@gmail.com>
2014-09-16 18:11:43 +0800

    Move send validation logic upstream

--- app/lib/wallet/test/validator.js
+++ app/lib/wallet/test/validator.js
@@ -1,125 +1,96 @@
-var Wallet = require('bitcoinjs-lib').Wallet
-var networks = require('bitcoinjs-lib').networks
+var Wallet = require('cb-wallet')
 var validateSend = require('../validator')
+var sinon = require('sinon')
 
 describe('validate', function(){
-  var wallet = new Wallet(new Buffer(128/8), networks.testnet)
-  var to = 'mqMRh2h5QbLyD4K3jZQMk2DzRwf2GwSoSQ'
-  var from = wallet.generateAddress()
-  var utxo = {
-    "hash":"6a4062273ac4f9ea4ffca52d9fd102b08f6c32faa0a4d1318e3a7b2e437bb9c7",
-    "outputIndex": 0,
-    "address" : from,
-    "value": 29999
-  }
-  wallet.setUnspentOutputs([utxo])
+  var wallet = new Wallet()
+  wallet.networkName = 'testnet'
+  var to = 'toaddress'
+  var amount = 0.000123
 
-  var fundsUnavailableMessage = "Some funds are temporarily unavailable. To send this transaction, you will need to wait for your pending transactions to be confirmed first (this should not take more than a few minutes)."
-  var fundsUnavailableLink = "https://github.com/hivewallet/hive-osx/wiki/Sending-Bitcoin-from-a-pending-transaction"
+  afterEach(function() {
+    Wallet.prototype.createTx.restore()
+  })
 
   describe('address', function(){
+    beforeEach(function() {
+      sinon.stub(Wallet.prototype, "createTx").throws(new Error('Invalid address'))
+    })
+
     it('catches invalid address', function(done){
-      validateSend(wallet, '123', 0, function(err){
+      validateSend(wallet, to, amount, function(err){
         expect(err.message).toEqual('Please enter a valid address to send to')
-        done()
-      })
-    })
-
-    it('catches address with the wrong version', function(done){
-      validateSend(wallet, 'LNjYu1akN22USK3sUrSuJn5WoLMKX5Az9B', 0, function(err){
-        expect(err.message).toEqual('Please enter a valid address to send to')
-        done()
-      })
-    })
-
-    it('allows valid pubKeyHash address', function(done){
-      validateSend(wallet, to, 0.0001, function(err){
-        expect(err).toBeNull()
-        done()
-      })
-    })
-
-    it('allows valid p2sh address', function(done){
-      validateSend(wallet, '2MvR3wixpB1usCNRugN6ufwxfT4GEFxoRhQ', 0.0001, function(err){
-        expect(err).toBeNull()
         done()
       })
     })
   })
 
-  describe('send amount', function(){
-    it('must be above dust threshold', function(done){
-      validateSend(wallet, to, 0.00000546, function(err){
+  describe('send amount not above dust threshold', function(){
+    beforeEach(function() {
+      sinon.stub(Wallet.prototype, "createTx").throws(new Error('Invalid value'))
+    })
+
+    it('works', function(done){
+      validateSend(wallet, to, amount, function(err){
         expect(err.message).toEqual('Please enter an amount above')
         done()
       })
     })
+  })
 
-    it('catches amount more than wallet balance', function(done){
-      validateSend(wallet, to, 0.0003, function(err){
-        expect(err.message).toEqual("You do not have enough funds in your wallet")
-        done()
+  describe('insufficient funds', function(){
+    function insufficientFundsError() {
+      var error = new Error('Insufficient funds')
+      error.needed = 13300
+      error.has = 12300
+      return error
+    }
+
+    describe('when it is well over wallet balance', function(){
+      beforeEach(function() {
+        sinon.stub(Wallet.prototype, "createTx").throws(insufficientFundsError())
+        sinon.stub(Wallet.prototype, "getBalance").returns(10000)
       })
+
+      it('produces an appropriate error message', function(done){
+        validateSend(wallet, to, amount, function(err){
+          expect(err.message).toEqual('You do not have enough funds in your wallet')
+          done()
+        })
+      })
+
+      afterEach(function(){ Wallet.prototype.getBalance.restore() })
     })
 
-    describe('trying to empty wallet without including fee', function(){
+    describe('when user attempts to empty wallet without including fee', function(){
+      beforeEach(function() {
+        sinon.stub(Wallet.prototype, "createTx").throws(insufficientFundsError())
+        sinon.stub(Wallet.prototype, "getBalance").returns(12300)
+      })
       var sendableAmountMessage = "It seems like you are trying to empty your wallet. Taking transaction fee into account, we estimated that the max amount you can send is. We have amended the value in the amount field for you"
 
-      it('amount equals balance exactly', function(done){
-        validateSend(wallet, to, 0.00029999, function(err){
+      it('produces an appropriate error message', function(done){
+        validateSend(wallet, to, amount, function(err){
           expect(err.message).toEqual(sendableAmountMessage)
-          expect(err.interpolations.sendableBalance).toEqual(0.00019999)
+          expect(err.interpolations.sendableBalance).toEqual(0.000113)
           done()
         })
       })
 
-      it('balance - feePerKb < amount < balance', function(done){
-        validateSend(wallet, to, 0.0002, function(err){
-          expect(err.message).toEqual(sendableAmountMessage)
-          expect(err.interpolations.sendableBalance).toEqual(0.00019999)
-          done()
-        })
-      })
-
-      it('asks user to try later when there is pending utxo', function(done){
-        var pendingUtxo = cloneObject(utxo)
-        pendingUtxo.pending = true
-        pendingUtxo.outputIndex = 1
-        pendingUtxo.value = 0 // lazy: does not need to reset after test
-
-        wallet.setUnspentOutputs([utxo, pendingUtxo])
-
-        validateSend(wallet, to, 0.0002, function(err){
-          expect(err.message).toEqual(fundsUnavailableMessage)
-          expect(err.href).toContain(fundsUnavailableLink)
-          done()
-        })
-      })
+      afterEach(function(){ Wallet.prototype.getBalance.restore() })
     })
 
-    it('returns fee when amount is valid', function(done){
-      validateSend(wallet, to, 0.0001, function(err, fee){
-        expect(err).toBeNull()
-        expect(fee).toEqual(0.0001)
-        done()
+    describe('when pending balance is sufficient to cover the transaction', function(){
+      beforeEach(function() {
+        var error = insufficientFundsError()
+        error.details = "Additional funds confirmation pending"
+        sinon.stub(Wallet.prototype, "createTx").throws(error)
       })
-    })
+      var fundsUnavailableMessage = "Some funds are temporarily unavailable. To send this transaction, you will need to wait for your pending transactions to be confirmed first (this should not take more than a few minutes)."
+      var fundsUnavailableLink = "https://github.com/hivewallet/hive-osx/wiki/Sending-Bitcoin-from-a-pending-transaction"
 
-    describe('when confirmed balance < amount + fee <= balance', function(){
-      beforeEach(function(){
-        var pendingUtxo = cloneObject(utxo)
-        pendingUtxo.pending = true
-        pendingUtxo.outputIndex = 1
-
-        wallet.setUnspentOutputs([utxo, pendingUtxo])
-      })
-
-      afterEach(function(){
-        wallet.setUnspentOutputs([utxo])
-      })
-
-      it('prompt user to wait', function(done){
-        validateSend(wallet, to, 0.00049998, function(err){
+      it('produces an appropriate error message', function(done){
+        validateSend(wallet, to, amount, function(err){
           expect(err.message).toEqual(fundsUnavailableMessage)
           expect(err.href).toContain(fundsUnavailableLink)
           done()
@@ -127,9 +98,4 @@ describe('validate', function(){
       })
     })
   })
-
-  // quick and dirty: does not deal with functions on object
-  function cloneObject(obj){
-    return JSON.parse(JSON.stringify(obj))
-  }
 })
